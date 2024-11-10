58424b95b9ccde25ff38ab43c3de1eb33193f41a
Sirinemn <sirinemn@outlook.fr>
2024-11-10 13:55:28 +0100

    setup tips component

--- src/app/tips/tips.component.ts
+++ src/app/tips/tips.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-tips',
   standalone: true,
   imports: [HeaderComponent,
-    RouterModule
+    RouterModule,
+    NgIf
   ],
   templateUrl: './tips.component.html',
   styleUrl: './tips.component.css'
 })
 export class TipsComponent {
+
+  public more:boolean = false;
+
+  moreTips() {
+    this.more = true;
+  }
+  lessTips() {
+    this.more = false;
+  }
 
 }
