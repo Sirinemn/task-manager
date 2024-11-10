83c43f88f7a6525e3ad27cba8ab6c26939a2e4c1
Sirinemn <sirinemn@outlook.fr>
2024-11-10 10:46:40 +0100

    update formcomponent

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,19 +1,31 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
+    ReactiveFormsModule,
+    NgIf
   ],
-  providers: [
-    FormGroup,
-    FormBuilder
-  ],
+
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
   public form!: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.form = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      priority: [false],
+      accomplished: [false],
+      description: ['', Validators.required]
+    })
+  }
 
 }
