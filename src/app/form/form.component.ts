1428137c9469c283812c86f30071c15f9d142a1c
Sirinemn <sirinemn@outlook.fr>
2024-11-11 10:18:59 +0100

    add Submit methode

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,7 +2,9 @@ import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderComponent } from "../header/header.component";
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Task } from '../interface/Task.interface';
+import { TaskServiceService } from '../service/task-service.service';
 
 @Component({
   selector: 'app-form',
@@ -21,7 +23,7 @@ export class FormComponent {
 
   public form!: FormGroup;
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder, private taskService: TaskServiceService, private router: Router){
     this.form = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
       priority: [false],
@@ -31,7 +33,17 @@ export class FormComponent {
   }
 
   onSubmit() {
-    throw new Error('Method not implemented.');
+    if (this.form.valid) { 
+      const newTask: Task = { 
+        id: Date.now(), 
+        title: this.form.get('title')?.value, 
+        description: this.form.get('description')?.value, 
+        prioritaire: this.form.get('priority')?.value, 
+       accompli: this.form.get('accomplished')?.value
+      };
+      this.taskService.addTask(newTask);
+      this.form.reset();
+      this.router.navigate(["/"]);
     }
-
+  }
 }
